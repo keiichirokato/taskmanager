c19e6ed2fdb868fccb66eebb10ced5c9a684bae8
keiichirokato <keiichiro.kato@playground.com>
2018-10-21 15:33:23 +0900

    タスクの新規登録画面を作成（tasks/new）

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,14 @@ class TasksController < ApplicationController
     def show
         @tasks = Task.all
     end
+
+    def new
+        @task = Task.new
+    end
+
+    def create
+        @task = Task.new(params.require(:task).permit(:name, :detail))
+        @task.save
+        redirect_to tasks_path
+    end
 end
