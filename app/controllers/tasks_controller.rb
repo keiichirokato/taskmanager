0e18bc073e2f2a631d301dbd718139b6c014bb1f
keiichirokato <keiichiro.kato@playground.com>
2018-10-21 16:29:48 +0900

    タスクの削除機能を作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,6 +23,12 @@ class TasksController < ApplicationController
         redirect_to tasks_path
     end
 
+    def destroy
+        @task = find_task_by_id
+        @task.destroy
+        redirect_to tasks_path
+    end
+
     private
     def task_params
         params.require(:task).permit(:name, :detail)
