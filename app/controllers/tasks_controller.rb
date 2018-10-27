b31493284006140d3256df6f83af1e7138479fa9
keiichirokato <keiichiro.kato@playground.com>
2018-10-28 06:39:25 +0900

    タスク終了期限を参照・登録できるよう対応

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -38,7 +38,7 @@ class TasksController < ApplicationController
 
     private
     def task_params
-        params.require(:task).permit(:name, :detail)
+        params.require(:task).permit(:name, :detail, :deadline_at)
     end
     
     def find_task_by_id
