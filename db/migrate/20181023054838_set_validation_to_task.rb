39775a850bea05d78662aa584af2bc058838522c
keiichirokato <keiichiro.kato@playground.com>
2018-10-27 19:24:30 +0900

    validationのmigrateを実行

--- db/migrate/20181023054838_set_validation_to_task.rb
+++ db/migrate/20181023054838_set_validation_to_task.rb
@@ -1,4 +1,6 @@
 class SetValidationToTask < ActiveRecord::Migration[5.2]
   def change
+    change_column :tasks, :name, :string, null: false, limit: 255
+    change_column :tasks, :detail, :string, limit: 10000
   end
 end
